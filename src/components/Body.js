71b52c8d8f36f45a206278a49aaf41322e5501a7
suchita gunaga <suchitagunaga.suchi@gmail.com>
2024-01-13 00:45:02 +0530

    used live api data instead of mockdata

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,11 +3,25 @@ import resObj from "../utils/mockData";
 import restaurants from "../utils/mockData";
 import { useEffect, useState } from "react";
 const Body = () => {
-  const [listOfRestaurants, setlistOfRestaurants] = useState(restaurants);
+  const [listOfRestaurants, setlistOfRestaurants] = useState([]);
   // useEffect(() => {
   //   let resObj1 = resObj.restaurants.filter((res) => res.info.avgRating > 4.5);
   //   console.log(resObj1);
   // }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0392092&lng=77.608586&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(json);
+    setlistOfRestaurants(
+      json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
+    );
+  };
 
   return (
     <div className="body">
